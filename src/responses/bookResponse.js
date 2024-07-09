286f02bfc073476f7e38dfaf5aa53bc7ad8c8f52
mytran324 <92320923+mytran324@users.noreply.github.com>
2024-07-09 14:46:12 +0700

    update

--- src/responses/bookResponse.js
+++ src/responses/bookResponse.js
@@ -1,12 +1,12 @@
 class BookResponse {
-  constructor(id, author, price, title, create_at, update_at, totalBook) {
+  constructor(id, author, price, title, view, create_at, update_at) {
     (this.id = id),
       (this.author = author),
       (this.price = price),
       (this.title = title),
+      (this.view = view),
       (this.create_at = create_at),
-      (this.update_at = update_at),
-      (this.totalBook = totalBook);
+      (this.update_at = update_at);
   }
 }
 
